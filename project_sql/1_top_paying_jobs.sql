f53aeb0578d2d18cb6c88fa30d68f34b84e368ca
manyasethi-27 <manya.sethi_cs23@gla.ac.in>
2026-09-07 01:00:19 +0530

    added readme and images

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -1,11 +1,14 @@
-SELECT name AS company_name, job_id, job_title, 
-job_location, job_schedule_type,
-salary_year_avg, job_posted_date
-FROM job_postings_fact j
-LEFT JOIN company_dim c
-ON j.company_id = c.company_id
-WHERE job_title_short = 'Data Analyst' 
-AND salary_year_avg IS NOT NULL
-AND job_location = 'Anywhere'
-ORDER BY salary_year_avg DESC
+SELECT 
+    name AS company_name, job_id, job_title, 
+    job_location, job_schedule_type,
+    salary_year_avg, job_posted_date
+FROM 
+    job_postings_fact j
+LEFT JOIN company_dim c ON j.company_id = c.company_id
+WHERE 
+    job_title_short = 'Data Analyst' 
+    AND salary_year_avg IS NOT NULL
+    AND job_location = 'Anywhere'
+ORDER BY 
+    salary_year_avg DESC
 LIMIT 10;
